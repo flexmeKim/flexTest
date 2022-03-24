f484c02002194fa54eefcce2197e6c0617098d43
minjin <kindcode@flexme.com>
2022-03-24 11:53:43 +0900

    ad
    ad

--- web_temp_suspension/lib/main.dart
+++ web_temp_suspension/lib/main.dart
@@ -1,12 +1,15 @@
-import 'dart:html';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:web_temp_suspension/view/temp_suspension_page.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
-  runApp(const MyApp());
+  // Here we set the URL strategy for our web app.
+  // It is safe to call this function when running on mobile or desktop as well.
+  setPathUrlStrategy();
+  runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
