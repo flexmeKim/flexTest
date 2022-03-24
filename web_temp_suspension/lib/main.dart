946dfecdd58331bdd5875d71378a4a0096320dd6
minjin <kindcode@flexme.com>
2022-03-24 11:34:24 +0900

    1
    1

--- web_temp_suspension/lib/main.dart
+++ web_temp_suspension/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "flexme_test",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'noto_sans_kr',
